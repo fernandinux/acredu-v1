171305079a97269a3b4260717a4b7d7b34e99e57
fernandinux <fernando.paca@gmail.com>
2021-01-03 18:32:15 -0500

    agregando post-thumbnails faltaba su action

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ function init_template(){
     add_theme_support( 'title-tag' );
 
 }
+add_action( 'after_setup_theme', 'init_template' );
 /**
  * Include CSS files
  */
@@ -53,8 +54,6 @@ function certificate_proyetech(){
 
 add_action( 'init', 'certificate_proyetech');
 
-if ( function_exists( 'add_theme_support' ) ) { 
-  add_theme_support( 'post-thumbnails' ); 
-}
+
 
 ?>
