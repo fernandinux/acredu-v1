1d575b221911593790c85503850dcb1ae389ba7f
fernandinux <fernando.paca@gmail.com>
2021-01-03 19:07:54 -0500

    enlace css sin /

--- header.php
+++ header.php
@@ -7,8 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <script src="https://kit.fontawesome.com/35d6a5945d.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="/css/animations.css">
-    <link rel="stylesheet" href="/css/styleacredu.css">
+    <link rel="stylesheet" href="css/animations.css">
+    <link rel="stylesheet" href="css/styleacredu.css">
     <!-- Parametros facebook -->
     <meta property="og:url"
         content="acredu.org" />    
