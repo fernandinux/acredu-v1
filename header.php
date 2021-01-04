4db7ebdaa5f6565de9a9f6d433ee722c44e5a4c0
fernandinux <fernando.paca@gmail.com>
2021-01-04 01:59:18 -0500

    correccion og head

--- header.php
+++ header.php
@@ -10,13 +10,13 @@
     <link rel="stylesheet" href="https://app.acredu.org/wp-content/themes/acredu-v1/css/animations.css">
     <link rel="stylesheet" href="https://app.acredu.org/wp-content/themes/acredu-v1/css/styleacredu.css">
     <link rel="stylesheet"  href="https://app.acredu.org/wp-content/themes/acredu-v1/singlestyle.css" type="text/css" media="all">
-    <!-- Parametros facebook -->
-    <meta property="og:url"
-        content="acredu.org" />    
-    <meta property="og:title" content="Certificados en linea" />
-    <meta property="og:description" content="aCredu certificados digitales en linea para tus estudiantes " />
-    <meta property="og:image"
-        content="https://acredu.netlify.app/asset/img/favicon.png" />
+   <!-- og para compartir -->
+    <meta property="og:url" content="<?php the_permalink(); ?>" />
+    <meta property="og:title" content="<?php the_field('name_event'); ?>" />
+    <meta property="og:description" content="Certificado digital de aCredu para <?php the_field('empresa_emisora'); ?>" />
+    <meta property="og:image" content="<?php the_post_thumbnail_url('full'); ?>" />
+    <meta name="theme-color" content="#013155" />
+<link rel="canonical" href="https://acredu.org/" />
     <title>
         <?php bloginfo( 'name'); ?>
     </title>
