5ba617da9141ec48f59d1810abdb65ecb1509faf
fernandinux <fernando.paca@gmail.com>
2021-01-15 21:04:20 -0500

    editando title en el share linkedin

--- single-imce.php
+++ single-imce.php
@@ -31,7 +31,7 @@
 			
 				<div class="row d-flex justify-content-center">
 					<a id="shareface" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"></i></a>
-					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=PLANIFICACIÓN_Y_CONTROL_DE_PROYECTOS_CON_PRIMAVERA_P6"><i class="fa fa-linkedin"></i></a>
+					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=Autorregulaci%C3%B3n%20y%20Funciones%20Ejecutivas:%20en%20el%20desarrollo%20de%20habilidades%20para%20toda%20la%20vida"><i class="fa fa-linkedin"></i></a>
 					<a id="sharepdf" target="_blank" href="https://app.acredu.org/wp-content/uploads/2021/01/<?php $imageContent = get_the_content();
 						$stripped = strip_tags($imageContent, '<p>'); //quitar etiquetas
 						echo $stripped; ?>.pdf"><i class="fa fa-download"></i></a>
