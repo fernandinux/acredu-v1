b779cdc31606b0e97857b60bfe10a81f40842c8b
fernandinux <fernando.paca@gmail.com>
2021-02-14 00:31:11 -0500

    code on page verificador2

--- page-verificador.php
+++ page-verificador.php
@@ -36,33 +36,27 @@ en page verificador
 					<?php
 					get_search_form();
 
-					the_widget( 'WP_Widget_Recent_Posts' );
+					// the_widget( 'WP_Widget_Recent_Posts' );
 					?>
 
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'draft-acredu' ); ?></h2>
-						<ul>
+					<!-- <div class="widget widget_categories">
+						<h2 class="widget-title"></h2>
+						<ul> -->
 							<?php
-							wp_list_categories(
-								array(
-									'orderby'    => 'count',
-									'order'      => 'DESC',
-									'show_count' => 1,
-									'title_li'   => '',
-									'number'     => 10,
-								)
-							);
+							// wp_list_categories(
+							// 	array(
+							// 		'orderby'    => 'count',
+							// 		'order'      => 'DESC',
+							// 		'show_count' => 1,
+							// 		'title_li'   => '',
+							// 		'number'     => 10,
+							// 	)
+							// );
 							?>
-						</ul>
-					</div><!-- .widget -->
+						<!-- </ul>
+					</div>.widget -->
 
-					<?php
-					/* translators: %1$s: smiley */
-					$draft_acredu_archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'draft-acredu' ), convert_smilies( ':)' ) ) . '</p>';
-					the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$draft_acredu_archive_content" );
-
-					the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
+					
 
 			</div><!-- .page-content -->
 
