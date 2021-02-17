e366005f592d39fab258df08ebd9c06b32d85bf0
fernandinux <fernando.paca@gmail.com>
2021-02-17 01:23:23 -0500

    personalizando resultado de verificacion5

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -14,7 +14,7 @@ content search
 	<header class="entry-header">
 		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 		<?php printf( __( 'The post type is: %s', 'textdomain' ), get_post_type( get_the_ID() ) );?>
-		<?php if ( 'post' === get_post_type() ) : ?>
+		<?php if ( 'proyetech' === get_post_type() ) : ?>
 		<div class="entry-meta">
 			<?php
 			
