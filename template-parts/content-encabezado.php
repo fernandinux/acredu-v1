cf06be33136a864e6d5f1f558a193c4c8fd9458a
fernandinux <fernando.paca@gmail.com>
2021-02-20 10:48:48 -0500

    agregando el encabezado con estilo md

--- template-parts/content-encabezado.php
+++ template-parts/content-encabezado.php
@@ -1,1 +1,7 @@
-<img src="https://app.acredu.org/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="logo acredu" style="width: 50px">
+<nav class="navbar navbar-light ">
+  <div class="container">
+    <a class="navbar-brand" href="https://acredu.org">
+      <img src="https://app.acredu.org/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="" loading="lazy" height="30">
+    </a>
+  </div>
+</nav>
