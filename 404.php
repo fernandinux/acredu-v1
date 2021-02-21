9727c5b683c066f1ea35909150f14d60e4e9b5a6
fernandinux <fernando.paca@gmail.com>
2021-02-21 13:15:32 -0500

    agregando 404.php con encabezado

--- 404.php
+++ 404.php
@@ -9,12 +9,12 @@
 
 get_header();
 ?>
-
+	<?php get_template_part( 'template-parts/content', 'encabezado' );?>
 	<main>
-<div>
-	<img scr="https://app.acredu.org/wp-content/uploads/2021/02/diploam-fallado2.png" style="width:300px;margin:5rem auto"/>
-</div>
-		<div style="text-align:center">
+		<div style="width:300px;margin:8rem auto">
+			<img src="https://app.acredu.org/wp-content/uploads/2021/02/diploam-fallado2.png" />
+		</div>
+		<div style="text-align:center;width:300px;margin:8rem auto">
 			<h3>Algo salio mal <i class="far fa-frown" style="font-size:15px"></i>, por favor, verifica el enlace al que quieres acceder</h3>
 		</div>
 
