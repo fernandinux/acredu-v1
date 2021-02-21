b69dc1448c644f9dfb266d4187a04252f1b1ba42
fernandinux <fernando.paca@gmail.com>
2021-02-21 15:10:58 -0500

    agregando pagina para recuperar  user

---
+++ page-forgotpassword.php
@@ -0,0 +1,11 @@
+<?php get_header();?>
+
+    <?php get_template_part( 'template-parts/content', 'encabezado' );?>
+
+	        
+    <?php the_content(); ?>
+	
+
+<div class="verifooter">
+    <?php get_footer(); ?>
+</div
