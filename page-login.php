238698f37e5457fe39332ce66a4d34165d56662b
fernandinux <fernando.paca@gmail.com>
2021-02-21 00:57:45 -0500

    agregando page-login  2

--- page-login.php
+++ page-login.php
@@ -1,5 +1,7 @@
 <?php get_header();?>
 
+    <?php get_template_part( 'template-parts/content', 'encabezado' );?>
+
 	<main class="w-50">
         
         <?php the_content(); ?>
--- searchform.php
+++ searchform.php
@@ -41,7 +41,7 @@
     placeholder="<?php echo esc_attr_x( 'ID Credential', 'placeholder' ) ?>"
     value="<?php echo get_search_query() ?>" 
     name="s" 
-    require
+    required
     />    
   </div>
 
