3009fe06da685eef14cb7d0492d6dd635d3dcb68
fernandinux <fernando.paca@gmail.com>
2021-02-20 23:12:08 -0500

    agregando datos del resultado de certificado 7

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -25,22 +25,22 @@
 			<?php the_title( sprintf( '<h2 class="card-title"><a href="%s" rel="bookmark"><i class="fas fa-graduation-cap" style="color:#007bff;font-size:29px;margin-right:5px" aria-hidden="true"></i>', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 			
 			<p>Información del certificado:</p>
-			<p class="card-text">
+			<p class="card-text mb-0">
 				<i class="fas fa-check-circle" style="font-size:18px;color:#32c132;"></i>
 			  Institución verificada por aCredu:<br>
 			</p>
-			<div style="width:100%;text-align:center"><?php the_field('empresa_emisora'); ?></div>
-			<p class="card-text">
+			<div style="width:100%;text-align:center;width:100%;text-align:center;margin: 0px auto 1rem auto;"><?php the_field('empresa_emisora'); ?></div>
+			<p class="card-text mb-0">
 				<i class="fas fa-check-circle" style="font-size:18px;color:#32c132;"></i>
 			El curso registrado es:<br>
 			</p>
-			<div style="width:100%;text-align:center"><?php the_field('name_event'); ?></div>
-			<p class="card-text">
+			<div style="width:100%;text-align:center;width:100%;text-align:center;margin: 0px auto 1rem auto;"><?php the_field('name_event'); ?></div>
+			<p class="card-text mb-0">
 				<i class="fas fa-check-circle" style="font-size:18px;color:#32c132;"></i>
 			Este certificado se emitió:<br>
 			</p>
-			<div style="width:100%;text-align:center"><?php the_field('fechainicio'); ?></div>
+			<div style="width:100%;text-align:center;width:100%;text-align:center;margin: 0px auto 1rem auto;"><?php the_field('fechainicio'); ?></div>
 			
-			<a href="htpps://acredu.org" class="btn" style="background:#03AA99"><i class="far fa-comment" style="font-size:18px;margin-right:5px"></i>Contactar al emisor</a>
+			<a href="htpps://acredu.org" class="btn mt-5" style="background:#03AA99"><i class="far fa-comment" style="font-size:18px;margin-right:5px"></i>Contactar al emisor</a>
 		</div><!-- .entry-header -->
 </article><!-- #post-<?php the_ID(); ?> -->
