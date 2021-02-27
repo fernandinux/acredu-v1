b5380af5afcc023d547106f727b079c8dc4854a1
fernandinux <fernando.paca@gmail.com>
2021-02-26 23:01:09 -0500

    add imagen

--- functions.php
+++ functions.php
@@ -142,12 +142,13 @@ function guardar_postulante_por_cf7( $wpcf7 ) {
 
 	) );
 
+    $imagen_id= media_handle_upload(['posted_data']['file-142'],$postulante_id);
 	/*
 	 * Si no hubo ningún error guardando el postulante
 	 * podemos guardar su email en un campo post_meta.
 	 */
 	if( ! is_wp_error( $postulante_id ) ) {
-
+        update_post_meta( $postulante_id, '_thumbnail_id', $imagen_id );
         update_field( 'id', $formulario['posted_data']['number-900'], $postulante_id );
 		add_post_meta( $postulante_id, 'mgp_email', $formulario['posted_data']['email-postulante'] );
 
