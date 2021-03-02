bf2ba6c507813ec62eaf5815b3352b637ff5539b
fernandinux <fernando.paca@gmail.com>
2021-03-01 23:11:01 -0500

    dashboard add 11

--- page-dashboard.php
+++ page-dashboard.php
@@ -2,79 +2,50 @@
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
 
+    
+
+
+
+
     <div class="row row-cols-1 row-cols-md-3 g-4 p-5">
         <div class="col my-5" style="max-width:300px">
             <div class="card h-200">
-                <img
+
+                <?php query_posts(array('post_type' => 'cursoscolectivo','orderby' => 'DESC')); ?>
+                       <?php if(have_posts()) : while(have_posts()) : the_post();?>
+                    
+                            <?php the_post_thumbnail(array( 'class' => 'card-img-top' )); ?>
+                            <?php //the_post_thumbnail(); ?>
+                                
+                                
+                            <div class="card-body">
+                                <h5 class="card-title"><?php the_title(); ?></h5>
+                                <p class="card-text">
+                                <?php the_content(); ?>
+                                </p>
+                                <a href="#!" class="btn" style="background:#11af88">Agregar Lista</a>
+                            </div>
+
+
+                                
+
+                        <?php endwhile; ?>
+                        <?php else:?>
+                        // no posts found
+                       <?php endif; wp_reset_query();
+                            ?>
+
+
+                <!-- <img
                     src="https://app.acredu.org/wp-content/uploads/2021/03/addCertif.jpg"
                     class="card-img-top"
                     alt="..."
-                />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                    </p>
-                    <a href="#!" class="btn" style="background:green">Button</a>
-                </div>
+                /> -->
+                
             </div>
         </div>
         <!-- card -->
-        <div class="col my-5" style="max-width:300px">
-            <div class="card h-200">
-                <img
-                    src="https://app.acredu.org/wp-content/uploads/2021/03/addCertif.jpg"
-                    class="card-img-top"
-                    alt="..."
-                />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                </div>
-            </div>
-        </div>
-        <!-- card -->
-        <div class="col my-5" style="max-width:300px">
-            <div class="card h-200">
-                <img
-                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
-                    class="card-img-top"
-                    alt="..."
-                />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                </div>
-            </div>
-        </div>
-        <!-- card -->
-        <div class="col my-5" style="max-width:300px">
-            <div class="card h-200">
-                <img
-                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
-                    class="card-img-top"
-                    alt="..."
-                />
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to
-                    additional content. This content is a little bit longer.
-                    </p>
-                    <a href="#!" class="btn btn-primary">Button</a>
-                </div>
-            </div>
-        </div>
-        <!-- card -->
+        
         <div class="col my-5" style="max-width:300px">
             <div class="card h-200 text-center py-5">
                 <a href="https://google.com" ><i class="far fa-plus-square" style="font-size:10rem;color:#80808059"></i></a>
