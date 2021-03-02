696e28aae62bf37849f9bb84b5585621f19c84db
fernandinux <fernando.paca@gmail.com>
2021-03-01 21:52:19 -0500

    dashboard add 3

--- page-dashboard.php
+++ page-dashboard.php
@@ -2,70 +2,100 @@
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
 
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-  <div class="col">
-    <div class="card h-100">
-      <img
-        src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
-        </p>
-        <a href="#!" class="btn btn-primary">Button</a>
-      </div>
+    <div class="row row-cols-1 row-cols-md-3 g-4 p-5">
+        <div class="col my-5">
+            <div class="card h-200">
+                <img
+                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
+                    class="card-img-top"
+                    alt="..."
+                />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                    <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div>
+        <!-- card -->
+        <div class="col my-5">
+            <div class="card h-200">
+                <img
+                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
+                    class="card-img-top"
+                    alt="..."
+                />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                    <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div>
+        <!-- card -->
+        <div class="col my-5">
+            <div class="card h-200">
+                <img
+                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
+                    class="card-img-top"
+                    alt="..."
+                />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                    <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div>
+        <!-- card -->
+        <div class="col my-5">
+            <div class="card h-200">
+                <img
+                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
+                    class="card-img-top"
+                    alt="..."
+                />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                    <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div>
+        <!-- card -->
+        <div class="col my-5">
+            <div class="card h-200">
+                <img
+                    src="https://mdbootstrap.com/img/new/standard/city/041.jpg"
+                    class="card-img-top"
+                    alt="..."
+                />
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">
+                    This is a longer card with supporting text below as a natural lead-in to
+                    additional content. This content is a little bit longer.
+                    </p>
+                    <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+            </div>
+        </div>
+        <!-- card -->
+  
+  
     </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img
-        src="https://mdbootstrap.com/img/new/standard/city/042.jpg"
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">This is a short card.</p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img
-        src="https://mdbootstrap.com/img/new/standard/city/043.jpg"
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content.
-        </p>
-      </div>
-    </div>
-  </div>
-  <div class="col">
-    <div class="card h-100">
-      <img
-        src="https://mdbootstrap.com/img/new/standard/city/044.jpg"
-        class="card-img-top"
-        alt="..."
-      />
-      <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          This is a longer card with supporting text below as a natural lead-in to
-          additional content. This content is a little bit longer.
-        </p>
-      </div>
-    </div>
-  </div>
-</div>
 	
 
 <div class="verifooter">
