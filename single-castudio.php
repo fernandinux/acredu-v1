f1b49b11f3bd43ce0c4c7f06d675618a09bed07c
fernandinux <fernando.paca@gmail.com>
2021-03-07 22:56:11 -0500

    castudio if condicional5

--- single-castudio.php
+++ single-castudio.php
@@ -92,7 +92,7 @@
                     	</iframe>
                     </div> -->
 				
-				<?php if (the_field('name_event')): ?>
+				<?php if (5=5): ?>
 					<?php echo ("Código que sea cierto Participacion"); ?>
 				<?php else: ?>
 					<?php echo ("Código que se ejecutará si no es cierto coo"); ?>					
