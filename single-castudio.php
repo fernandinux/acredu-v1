b029baff1d19ac2677b9ce5cde1713878a095d85
fernandinux <fernando.paca@gmail.com>
2021-03-07 22:52:18 -0500

    castudio if condicional2

--- single-castudio.php
+++ single-castudio.php
@@ -92,7 +92,7 @@
                     	</iframe>
                     </div> -->
 				
-				<?php if (the_field('name_event')=="Certificado de Participación"): ?>
+				<?php if (the_field('name_event')=='Certificado de Participación'): ?>
 					<?php echo ("Código que sea cierto Participacion"); ?>
 				<?php else: ?>
 					<?php echo ("Código que se ejecutará si no es cierto coo"); ?>					
