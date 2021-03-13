9187c8e777c31e8188c598c50211830ab2f7311e
fernandinux <fernando.paca@gmail.com>
2021-03-13 18:56:18 -0500

    cuando esta logueado que sale5

--- page-login.php
+++ page-login.php
@@ -4,7 +4,30 @@
 
    <?php if (is_user_logged_in()) :
      ?>
-     <p>Este mensaje sólo se mostrará en pantalla a los usuarios registrados e identificados. Los invitados no podrán verlo.</p>
+        <div class="m-5 d-flex flex-wrap">
+            <!-- Jumbotron -->
+            <div class="p-1 m-5 text-center bg-light">
+                <h3 class="my-3">Perfil</h3>
+                <p class="mb-3" style="max-width:200px;">Edita tu perfil de tu institución</p>
+                <a class="btn p-2" style="background:green;" href="" role="button">Editar</a>
+            </div>
+            <!-- Jumbotron -->
+            <!-- Jumbotron -->
+            <div class="p-1 m-5 text-center bg-light">
+                <h3 class="my-3">Cursos</h3>
+                <p class="mb-3" style="max-width:200px;">Revisa los cursos que has creado</p>
+                <a class="btn p-2" style="background:green;" href="" role="button">Revisar</a>
+            </div>
+            <!-- Jumbotron -->
+            <!-- Jumbotron -->
+            <div class="p-1 m-5 text-center bg-light">
+                <h3 class="my-3">Ayuda</h3>
+                <p class="mb-3" style="max-width:200px;">Solicita ayuda de uno de nuestros consultores</p>
+                <a class="btn p-2" style="background:green;" href="" role="button">Contactar</a>
+            </div>
+            <!-- Jumbotron -->
+        </div>
+
      <?php else :?>
         <?php the_content(); ?>
      <?php endif;?>
