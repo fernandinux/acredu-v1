ebd29bda9517db5804cc71424500437203d30e44
fernandinux <fernando.paca@gmail.com>
2021-03-13 18:25:09 -0500

    cuando esta logueado que sale2

--- page-login.php
+++ page-login.php
@@ -2,9 +2,11 @@
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
 
-    <?php if (is_user_logged_in() )?>
-    Hola
-    <?php endif?>
+   <?php if (is_user_logged_in()) :
+     ?>
+     <p>Este mensaje sólo se mostrará en pantalla a los usuarios registrados e identificados. Los invitados no podrán verlo.</p>
+     <?php
+endif;?>
 	        
     <?php the_content(); ?>
 	
