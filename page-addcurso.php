a7bca4014ac8a603b54fcfeba0a2dfe1d266df42
fernandinux <fernando.paca@gmail.com>
2021-03-14 22:34:01 -0500

    addcurso prueba user email

--- page-addcurso.php
+++ page-addcurso.php
@@ -11,7 +11,7 @@
                <?php  
                $memberID = get_current_user_id();
                $memberInfo = get_userdata($memberID);
-               $memberName = $memberInfo->first_name;
+               $memberName = $memberInfo->email;
                echo $memberName;
                ?> 
         </div>
