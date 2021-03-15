e15b1fc1402ff60eace34ee47290119c16eaeebf
fernandinux <fernando.paca@gmail.com>
2021-03-14 22:20:49 -0500

    addcurso prueba user

--- page-addcurso.php
+++ page-addcurso.php
@@ -8,10 +8,15 @@
             <!-- Jumbotron -->
             <?php the_content(); ?>
             <!-- Jumbotron -->
+               <?php  
+               $memberID = get_current_user_id();
+               $memberInfo = get_userdata($memberID);
+               echo($memberInfo);
+               ?> 
         </div>
 
      <?php else :?>
-        
+        <h2 class="my-5 mx-auto">Para acceder a esta pagina necesita autenticarse</h2>
      <?php endif;?>
 	        
     
