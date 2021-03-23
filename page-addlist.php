8aaf16229ebabcd038575b934b6a903c2d237ff3
fernandinux <fernando.paca@gmail.com>
2021-03-23 05:03:28 -0500

    add addlist 3

--- page-addlist.php
+++ page-addlist.php
@@ -2,7 +2,12 @@
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
 <div style="max-width:300px; margin:10px auto">
-
+<div class="text-center"> 
+<img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px">
+<a class="btn btn-primary" style="background-color: #3b5998" href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button"
+  ><i class="fas fa-download"></i>Descargar plantilla csv
+></a>
+</div>
 <?php the_content(); ?>
 
 </div>
