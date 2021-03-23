a3bd4b0822c58b6491390e62178de3c6858bf92e
fernandinux <fernando.paca@gmail.com>
2021-03-23 12:57:09 -0500

    add logout button3

--- template-parts/content-encabezado.php
+++ template-parts/content-encabezado.php
@@ -3,6 +3,6 @@
     <a class="navbar-brand" href="https://acredu.org">
       <img src="https://app.acredu.org/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="" loading="lazy" height="30">
     </a>
-    <a class="nav-link btn btn-outline-success btn-sm" data-mdb-ripple-color="dark" style="color:#03aa99 ! important;" href="https://app.acredu.org/wp-login.php?action=logout">Cerrar sesión</a>
+    <a class="nav-link btn btn-outline-success btn-sm" data-mdb-ripple-color="dark" style="color:#03aa99 ! important; border: 2px solid #03aa99 !important;" href="https://app.acredu.org/wp-login.php?action=logout">Cerrar sesión</a>
   </div>
 </nav>
