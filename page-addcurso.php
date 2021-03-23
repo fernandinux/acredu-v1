2e494f1913f5e5d376406d24ad949566b0c81b89
fernandinux <fernando.paca@gmail.com>
2021-03-23 05:34:06 -0500

    add addlist 5 y add curso3

--- page-addcurso.php
+++ page-addcurso.php
@@ -10,7 +10,7 @@
                <a href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button">
                <img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
               <br>
-                  <div class="btn btn-primary waves-effect waves-light" style="background-color: #1BB27C">
+                  <div class="btn  waves-effect waves-light" style="background-color: #1BB27C">
                <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
             </div>
             <?php the_content(); ?>
--- page-addlist.php
+++ page-addlist.php
@@ -1,14 +1,15 @@
 <?php get_header();?>
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
-    <div class="text-center my-5"> 
+<div style="max-width:300px; margin:10px auto">
+        <div class="text-center my-5"> 
 
         <a href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button">
         <img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
         <div class="btn btn-primary waves-effect waves-light" style="background-color: #1BB27C">
         <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
-    </div>
-<?php the_content(); ?>
+        </div>
+    <?php the_content(); ?>
 
 </div>
    
