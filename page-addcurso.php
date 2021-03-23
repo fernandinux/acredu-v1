4aa0a4e46e902d8b51fb0b2570a8810e0b7872e0
fernandinux <fernando.paca@gmail.com>
2021-03-23 05:13:16 -0500

    add addlist 4 y add curso

--- page-addcurso.php
+++ page-addcurso.php
@@ -6,6 +6,12 @@
      ?>
         <div class="m-5 d-flex flex-wrap">
             <!-- Jumbotron -->
+            <div class="text-center my-5"> 
+               <a href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button">
+               <img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
+               <div class="btn btn-primary waves-effect waves-light" style="background-color: #3b5998">
+               <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
+            </div>
             <?php the_content(); ?>
             <!-- Jumbotron -->
                <?php  
--- page-addlist.php
+++ page-addlist.php
@@ -1,13 +1,13 @@
 <?php get_header();?>
 
     <?php get_template_part( 'template-parts/content', 'encabezado' );?>
-<div style="max-width:300px; margin:10px auto">
-<div class="text-center"> 
-<img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px">
-<a class="btn btn-primary" style="background-color: #3b5998" href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button"
-  ><i class="fas fa-download"></i>Descargar plantilla csv
-></a>
-</div>
+    <div class="text-center my-5"> 
+
+        <a href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button">
+        <img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
+        <div class="btn btn-primary waves-effect waves-light" style="background-color: #3b5998">
+        <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
+    </div>
 <?php the_content(); ?>
 
 </div>
