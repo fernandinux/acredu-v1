521d9183b8a509efc4928e7901113042aa09eb37
fernandinux <fernando.paca@gmail.com>
2021-03-23 09:04:07 -0500

    add theme csv

--- page-addcurso.php
+++ page-addcurso.php
@@ -6,13 +6,13 @@
      ?>
         <div class="m-5 d-flex flex-wrap">
             <!-- Jumbotron -->
-            <!-- <div class="text-center m-5"> 
+             <div class="text-center m-5"> 
                <a href="https://app.acredu.org/wp-content/uploads/2021/03/Plantilla-lista-de-beneficiarios.csv" role="button">
                <img src="https://app.acredu.org/wp-content/uploads/2021/03/1200px-.csv_icon.svg_.png" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
               <br>
                   <div class="btn  waves-effect waves-light" style="background-color: #1BB27C">
                <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
-            </div> -->
+            </div>
             <?php the_content(); ?>
             <!-- Jumbotron -->
                <?php  
