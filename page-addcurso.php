7e42aedeb99015479699d8e0cefbcdbbbc8c9b99
fernandinux <fernando.paca@gmail.com>
2021-03-31 16:43:29 -0500

    boton google sheet 2

--- page-addcurso.php
+++ page-addcurso.php
@@ -15,7 +15,7 @@
                <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
             </div>
             <div class="text-center m-5"> 
-               <a href="https://docs.google.com/spreadsheets/d/1lYlMDX3WLIlWx3TyGNphuzjVpccIiB-ZBDGleFr-fIA/edit?usp=sharing" role="button">
+               <a  target="_blank" href="https://docs.google.com/spreadsheets/d/1lYlMDX3WLIlWx3TyGNphuzjVpccIiB-ZBDGleFr-fIA/edit?usp=sharing" role="button">
                <img src="https://app.acredu.org/wp-content/uploads/2021/03/sheet.jpg" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
               <br>
                   <div class="btn  waves-effect waves-light" style="background-color: #1BB27C">
