4a1c568aac512f71fdecd8305624e9919b825608
fernandinux <fernando.paca@gmail.com>
2021-04-01 14:11:30 -0500

    cambio en miguelSierra agregar whatsapp debajo del certificado

--- single-miguelsierra.php
+++ single-miguelsierra.php
@@ -20,7 +20,9 @@
 		<div class="col-md-12 py-5 cloudy-knoxville-gradient">
 							
         	<?php the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); ?>
-						
+			<div style="display: flex;flex-direction: row;justify-content: center;align-items: center;margin: 20px auto 0px auto;">
+            <small>¿Algo salió mal con tu certificado?, escribenos:</small>
+            <a id="ayudabnp" target="_blank" href="https://bit.ly/2EIKmrc"><i class="fa fa-whatsapp faw"></i></a></div>			
 		</div>
 	</div>
 
