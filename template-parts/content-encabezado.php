46a1ae3c7a3c925843db22243e3968d155d9789f
fernandinux <fernando.paca@gmail.com>
2021-04-02 14:41:15 -0500

    add condicional para cerrar sesion

--- template-parts/content-encabezado.php
+++ template-parts/content-encabezado.php
@@ -3,6 +3,15 @@
     <a class="navbar-brand" href="https://acredu.org">
       <img src="https://app.acredu.org/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="" loading="lazy" height="30">
     </a>
-    <a class="nav-link btn btn-outline-success btn-sm" data-mdb-ripple-color="dark" style="color:#03aa99 ! important; border: 2px solid #03aa99 !important;" href="https://app.acredu.org/wp-login.php?action=logout">Cerrar sesión</a>
+    <?php
+        if (is_user_logged_in()){
+          ?>
+              <a class="nav-link btn btn-outline-success btn-sm" 
+                  data-mdb-ripple-color="dark" 
+                  style="color:#03aa99 ! important; border: 2px solid #03aa99 !important;" 
+                  href="https://app.acredu.org/?piereg_logout_url=true">Cerrar sesión</a>
+       <?php }
+       ?>
+
   </div>
 </nav>
