15acf39e3b3f8ce0d4cfd4ac92a79fbd225281cc
fernandinux <fernando.paca@gmail.com>
2021-04-02 18:03:07 -0500

    add post type colocado en el enlace de creacion de curso para que llene el text-validate del form agregando echo

--- page-dashboard.php
+++ page-dashboard.php
@@ -76,7 +76,7 @@
         
         <div class="col my-5" style="max-width:300px">
             <div class="card h-200 text-center py-5">
-                <a href="https://app.acredu.org/addcurso/?text-validate=<?php $posType ?>" ><i class="far fa-plus-square" style="font-size:10rem;color:#80808059"></i></a>
+                <a href="https://app.acredu.org/addcurso/?text-validate=<?php echo $posType ?>" ><i class="far fa-plus-square" style="font-size:10rem;color:#80808059"></i></a>
                 <div class="card-body">
                     <h5 class="card-title">Agregar Nuevo Curso</h5>                   
                 </div>
