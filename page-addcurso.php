e507a3ba38ac819de9d219080b7277d2238c9696
fernandinux <fernando.paca@gmail.com>
2021-04-02 20:45:21 -0500

    add post type 7 clientes primer condicional if de colectivo 12

--- page-addcurso.php
+++ page-addcurso.php
@@ -21,8 +21,6 @@
                <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Descargar plantilla csv </div></a>
 
                <a  target="_blank" href="https://docs.google.com/spreadsheets/d/1lYlMDX3WLIlWx3TyGNphuzjVpccIiB-ZBDGleFr-fIA/edit?usp=sharing" role="button">
-               <img src="https://app.acredu.org/wp-content/uploads/2021/03/sheet.jpg" alt="plantilla csv" style="width:100px" data-pagespeed-url-hash="3348433114" onload="pagespeed.CriticalImages.checkImageForCriticality(this);" class="">
-              <br>
                   <div class="btn  waves-effect waves-light" style="background-color: #1BB27C">
                <i aria-hidden="true" class="fas fa-download mr-2" style="font-size:12px; background-color:#1BB27C;"></i>Plantilla en google sheet </div></a>
 
