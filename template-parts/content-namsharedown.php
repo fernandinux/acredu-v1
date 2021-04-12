87b768a98b2042b685da0a8e9cf4115f5560c688
fernandinux <fernando.paca@gmail.com>
2021-04-11 21:36:55 -0500

    add content-certificate new button 3

--- template-parts/content-namsharedown.php
+++ template-parts/content-namsharedown.php
@@ -4,16 +4,16 @@
 <div class="row d-flex justify-content-center">
     <a id="shareface" target="_blank" 
         href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
-        <i class="fa fa-facebook">Compartir</i>
+        <i class="fa fa-facebook"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i>
     </a>
     <a id="sharelink" target="_blank" 
         onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" 
         href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=<?php the_field('name_event'); ?>">
-        <i class="fa fa-linkedin">Compartir</i>
+        <i class="fa fa-linkedin"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i>
     </a>
     <a id="sharepdf" target="_blank" 
         href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.pdf">
-        <i class="fa fa-download"> Descargar</i>
+        <i class="fa fa-download"><span style="font-size:15px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Descargar pdf</span></i>
     </a>
 
     
