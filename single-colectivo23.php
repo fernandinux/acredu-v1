307aca57294e52ff9bfec33a43f72c752660144c
fernandinux <fernando.paca@gmail.com>
2021-04-11 22:11:36 -0500

    add content-buttonlinkedin

--- single-colectivo23.php
+++ single-colectivo23.php
@@ -238,15 +238,7 @@
 		<div class="col-md-4 col-sm-12 text-center">
 			 
 			<!-- Inicio add Linkedin profile -->
-			<div class="button-linkedin-web zoom animated zoomIn slow pt-2">
-				<p class=" mb-0 mx-auto">Agrégalo con solo 2 clicks</p>
-				<a onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnp');" href="https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&amp;name=<?php the_field('name_event'); ?>&amp;organizationId=<?php the_field('id_linkedin'); ?>&amp;issueYear=<?php the_field('year_linkedin'); ?>&amp;issueMonth=<?php the_field('month_linkedin'); ?>&amp;certUrl=<?php the_permalink(); ?>&amp;certId=<?php the_field('id'); ?>">
-				<img class="my-3 z-depth-4" src="https://app.acredu.org/wp-content/uploads/2021/04/es_ES.png" alt="Agregar certificado LinkedIn" data-pagespeed-url-hash="3180379771" onload="pagespeed.CriticalImages.checkImageForCriticality(this);"></a>
-                <p>
-				Credential ID: <?php the_field('id'); ?>  
-			</p>
-			<div class="mb-5"><div class="text-center"><p class="mb-0">Emitido en:</p><p class="text-blue-proyetech"><?php the_field('fechainicio'); ?></p></div></div>
-            </div>
+			<?php get_template_part( 'template-parts/content', 'buttonlinkedin' );?>  
 			<!-- Fin add linkedin profile -->
 			
             <div style="margin: 20px auto;">
---
+++ template-parts/content-buttonlinkedin.php
@@ -0,0 +1,25 @@
+<div class="button-linkedin-web zoom animated zoomIn slow pt-2">
+				<p class=" mb-0 mx-auto">Agrégalo con solo 2 clicks</p>
+				<a href="https://www.linkedin.com/profile/add?startTask=CERTIFICATION_NAME&amp;
+                    name=<?php the_field('name_event'); ?>&amp;
+                    organizationId=<?php the_field('id_linkedin'); ?>&amp;
+                    issueYear=<?php the_field('year_linkedin'); ?>&amp;
+                    issueMonth=<?php the_field('month_linkedin'); ?>&amp;
+                    certUrl=<?php the_permalink(); ?>&amp;
+                    certId=<?php the_field('id'); ?>">
+                    
+                    <img class="my-3 z-depth-4" 
+                        src="https://bit.ly/2Qe6a3l" 
+                        alt="Agregar certificado LinkedIn" 
+                    >
+                </a>
+                <p>
+				    Credential ID: <?php the_field('id'); ?>  
+			    </p>
+                <div class="mb-5">
+                    <div class="text-center">
+                        <p class="mb-0">Emitido en:</p>
+                        <p class="text-blue-proyetech"><?php the_field('fechainicio'); ?></p>
+                    </div>
+                </div>
+    </div>
