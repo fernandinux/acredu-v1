6e62ee9ab4a2da666facac5aeccd8a1817a8766c
fernandinux <fernando.paca@gmail.com>
2021-04-11 21:14:10 -0500

    add content-certificate new button 2

--- template-parts/content-namsharedown.php
+++ template-parts/content-namsharedown.php
@@ -4,21 +4,18 @@
 <div class="row d-flex justify-content-center">
     <a id="shareface" target="_blank" 
         href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
-        <i class="fa fa-facebook"></i>
+        <i class="fa fa-facebook">Compartir</i>
     </a>
     <a id="sharelink" target="_blank" 
         onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" 
         href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=<?php the_field('name_event'); ?>">
-        <i class="fa fa-linkedin"></i>
+        <i class="fa fa-linkedin">Compartir</i>
     </a>
     <a id="sharepdf" target="_blank" 
         href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.pdf">
-        <i class="fa fa-download"></i>
+        <i class="fa fa-download"> Descargar</i>
     </a>
 
-    <a class="btn btn-primary" style="background-color: #3b5998" href="#!" role="button">
-    <i class="fab fa-facebook-f"></i>
-    Compartir
-    </a
+    
 >
 </div>
