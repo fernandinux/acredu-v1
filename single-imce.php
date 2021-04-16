254cf5c000078a2b8fa6347532ed3e1f85990003
fernandinux <fernando.paca@gmail.com>
2021-04-16 12:24:08 -0500

    correccion boton descargar de imce

--- single-imce.php
+++ single-imce.php
@@ -21,7 +21,25 @@
 
 	<div class="row text-blue-proyetech pt-3 align-middle">
 		<div class="col-md-8">			
-			<?php get_template_part( 'template-parts/content', 'namsharedown' );?> 
+			<div class="h2 text-center my-3"><?php the_field('name_event'); ?>
+			</div>
+			<div class="row d-flex justify-content-center">
+				<a id="shareface" target="_blank" 
+					href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>">
+					<i class="fa fa-facebook"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i>
+				</a>
+				<a id="sharelink" target="_blank" 
+					onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" 
+					href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=<?php the_field('name_event'); ?>">
+					<i class="fa fa-linkedin"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i>
+				</a>
+				<a id="sharepdf" target="_blank" 
+					href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php $imageContent = get_the_content();
+						$stripped = strip_tags($imageContent, '<p>'); //quitar etiquetas
+						echo $stripped; ?>.pdf">
+					<i class="fa fa-download"><span style="font-size:15px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Descargar pdf</span></i>
+				</a>
+			</div>
 			<hr style="height:1px;border-width:0;color:black;background-color:gray">
 			<div class="d-md-flex justify-content-sm-center">
 			
