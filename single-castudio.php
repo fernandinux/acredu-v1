d01f2a39730456ce25ad2f0c99302a6e737d21ab
fernandinux <fernando.paca@gmail.com>
2021-04-26 13:22:04 -0500

    agregando texto a botones de Castudio 3

--- single-castudio.php
+++ single-castudio.php
@@ -33,9 +33,9 @@
 			<div class="h2 text-center my-3"><?php the_field('name_event'); ?></div>
 			
 				<div class="row d-flex justify-content-center">
-					<a id="shareface" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook">Compartir</i></a>
-					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=<?php the_field('name_event'); ?>"><i class="fa fa-linkedin">Compartir</i></a>
-					<a id="sharepdf" target="_blank" href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.pdf"><i class="fa fa-download">Descargar</i></a>
+					<a id="shareface" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i></a>
+					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=<?php the_field('name_event'); ?>"><i class="fa fa-linkedin"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i></a>
+					<a id="sharepdf" target="_blank" href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.pdf"><i class="fa fa-download"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Descargar</span></i></a>
 				</div>
 			
 			<hr style="height:1px;border-width:0;color:black;background-color:gray">
