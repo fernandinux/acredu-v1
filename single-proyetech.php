72b5620ee1b27d6533823275da1356245191c327
fernandinux <fernando.paca@gmail.com>
2021-04-29 19:07:35 -0500

    correccion en single proyetech el nombre de los botones

--- single-proyetech.php
+++ single-proyetech.php
@@ -30,11 +30,11 @@
 			<div class="h2 text-center my-3"><?php the_field('name_event'); ?></div>
 			
 				<div class="row d-flex justify-content-center">
-					<a id="shareface" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"></i></a>
-					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=PLANIFICACIÓN_Y_CONTROL_DE_PROYECTOS_CON_PRIMAVERA_P6"><i class="fa fa-linkedin"></i></a>
+					<a id="shareface" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=<?php the_permalink(); ?>"><i class="fa fa-facebook"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i></a>
+					<a id="sharelink" target="_blank" onClick="_gaq.push(['_trackEvent', ' bnp', ‘Click', 'databnpshare');" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php the_permalink(); ?>/&title=Acredu&summary=PLANIFICACIÓN_Y_CONTROL_DE_PROYECTOS_CON_PRIMAVERA_P6"><i class="fa fa-linkedin"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Compartir</span></i></a>
 					<a id="sharepdf" target="_blank" href="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php $imageContent = get_the_content();
 						$stripped = strip_tags($imageContent, '<p>'); //quitar etiquetas
-						echo $stripped; ?>.pdf"><i class="fa fa-download"></i></a>
+						echo $stripped; ?>.pdf"><i class="fa fa-download"><span style="font-size:18px; margin: 0px 0px 0px 8px;font-family:Roboto; font-weight:bold">Descargar</span></i></a>
 				</div>
 			
 			<hr style="height:1px;border-width:0;color:black;background-color:gray">
