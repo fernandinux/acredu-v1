4a38389b411c00b01f977acb6b93122cdbe38aab
fernandinux <fernando.paca@gmail.com>
2021-05-28 23:36:08 -0500

    corregir la redireccion del dashboard 2

--- page-login.php
+++ page-login.php
@@ -4,28 +4,10 @@
 
    <?php if (is_user_logged_in()) :
      ?>
-        <div class="m-5 d-flex flex-wrap">
-            <!-- Jumbotron -->
-            <!-- <div class="p-1 m-5 text-center bg-light">
-                <h3 class="my-3">Perfil</h3>
-                <p class="mb-3" style="max-width:200px;">Edita tu perfil de tu institución</p>
-                <a class="btn p-2" style="background:green;" href="" role="button">Editar</a>
-            </div> -->
-            <!-- Jumbotron -->
-            <!-- Jumbotron -->
-            <div class="p-1 m-5 text-center bg-light">
-                <h3 class="my-3">Lista de Cursos</h3>
-                <p class="mb-3" style="max-width:200px;">Accede a la lista de cursos</p>
-                <a class="btn p-2" style="background:green;" href="/dashboard/" role="button">Ir al Dashboard</a>
-            </div>
-            <!-- Jumbotron -->
-            <!-- Jumbotron -->
-            <!-- <div class="p-1 m-5 text-center bg-light">
-                <h3 class="my-3">Ayuda</h3>
-                <p class="mb-3" style="max-width:200px;">Solicita ayuda de uno de nuestros consultores</p>
-                <a class="btn p-2" style="background:green;" href="" role="button">Contactar</a>
-            </div> -->
-            <!-- Jumbotron -->
+        <div class="m-5 text-center">
+            <br>
+            <h3 class="my-3">Lista de Cursos</h3><br>
+            <a class="btn p-2 waves-effect waves-light" href="/dashboard/" role="button" style="background:green;">Ir al Dashboard</a>
         </div>
 
      <?php else :?>
