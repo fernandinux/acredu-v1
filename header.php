4ecc628001d854be0ef35b2c03632d56a3bfc659
fernandinux <fernando.paca@gmail.com>
2021-06-20 09:14:56 -0500

    cambiando imagen de certi en landing en colectivo23 cambiando datos header

--- header.php
+++ header.php
@@ -14,13 +14,13 @@
     <link rel="stylesheet"  href="https://app.acredu.org/wp-content/themes/acredu-v1/singlestyle.css" type="text/css" media="all">
    <!-- og para compartir -->
     <meta property="og:url" content="<?php the_permalink(); ?>" />
-    <meta property="og:title" content="<?php the_field('name_event'); ?>" />
+    <meta property="og:title" content="Certificación en <?php the_field('name_event'); ?> • <?php the_title(); ?> • <?php the_field('empresa_emisora'); ?>" />
     <meta property="og:description" content="Certificado digital de aCredu para <?php the_field('empresa_emisora'); ?>"/>
     <meta property="og:image" content="<?php the_post_thumbnail_url('full'); ?>" />
     <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="1000" />
+    <meta property="og:image:height" content="630" />
     <meta name="theme-color" content="#013155" />
-    <link rel="canonical" href="https://acredu.org/" />
+    <link rel="canonical" href="https://acredu.co/" />
     <!-- <title>
          <?php /* bloginfo( 'name');*/ ?>
     </title> -->
