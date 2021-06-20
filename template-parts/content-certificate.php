39d15b1e242bd13133ded29fdbc8ec80ada18999
fernandinux <fernando.paca@gmail.com>
2021-06-20 07:47:15 -0500

    cambiando imagen de certi en landing en colectivo23

--- template-parts/content-certificate.php
+++ template-parts/content-certificate.php
@@ -1,7 +1,12 @@
 <div class="row">		
 		<div class="col-md-12 pt-5 cloudy-knoxville-gradient">
-							
-        	<?php the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); ?>
+
+			<img src="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.jpg" 
+			class="img-fluid shadow-lg mx-auto d-block animated zoomIn thumb-of-pdf wp-post-image" 
+			alt="certificado de  para <?php the_title(); ?>" 
+			loading="lazy" width="768" height="594">
+
+        	<?php /*the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); */?>
 			<div style="display: flex;flex-direction: row;justify-content: center;align-items: center;margin: 20px auto 0px auto;">
             <small>¿Algo salió mal con tu certificado?, escríbenos:</small>
             <a id="ayudabnp" target="_blank" href="https://bit.ly/2EIKmrc"><i class="fa fa-whatsapp faw"></i></a></div>			
