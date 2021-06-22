28a36f38194e0b75776c1266136850229b6a6d89
fernandinux <fernando.paca@gmail.com>
2021-06-21 22:47:05 -0500

    regresando el thumbanil a miguel sierra

--- single-miguelsierra.php
+++ single-miguelsierra.php
@@ -19,9 +19,9 @@
 	<div class="row">		
 		<div class="col-md-12 py-5 cloudy-knoxville-gradient">
 
-			<img src="https://app.acredu.org/wp-content/uploads/2021/<?php the_field('mespublicacion'); ?>/<?php the_field('id'); ?>.jpg" class="img-fluid shadow-lg mx-auto d-block animated zoomIn thumb-of-pdf wp-post-image" alt="certificado de  para <?php the_title(); ?>" loading="lazy" width="768" height="594">
+			
 
-        	<?php /*the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); */?>
+        	<?php the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); ?>
 			<div style="display: flex;flex-direction: row;justify-content: center;align-items: center;margin: 20px auto 0px auto;">
             <small>¿Algo salió mal con tu certificado?, escribenos:</small>
             <a id="ayudabnp" target="_blank" href="https://bit.ly/2EIKmrc"><i class="fa fa-whatsapp faw"></i></a></div>			
