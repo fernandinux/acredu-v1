d0719e93fbc66d8204cfbb31ec3ab24dd5e3f98b
fernandinux <fernando.paca@gmail.com>
2021-06-28 20:45:15 -0500

    agregando badge platilla 16

--- single-badgecolectivo23.php
+++ single-badgecolectivo23.php
@@ -5,13 +5,13 @@
 
 <main>
 	<div id="fb-root"></div>
-<script>(function(d, s, id) {
-var js, fjs = d.getElementsByTagName(s)[0];
-if (d.getElementById(id)) return;
-js = d.createElement(s); js.id = id;
-js.src = "https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0";
-fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+	<script>(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "https://connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v3.0";
+	fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script>
 
    
         
@@ -106,7 +106,61 @@ fjs.parentNode.insertBefore(js, fjs);
         
 </main>
 <div class="badgefooter">
-	<?php get_footer(); ?>
+	<footer class="page-footer font-small elegant-color-dark pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">  
+
+    <!-- Grid row -->
+    <div class="row d-flex align-items-center">
+
+      <!-- Grid column -->
+      <div class="col-md-7 col-lg-8">
+
+        <!--Copyright-->
+        <p class="text-center text-md-left">
+          <a href="https://acredu.org">Powered by aCredu © 2021 </a>
+        </p>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-5 col-lg-4 ml-lg-0">
+
+        <!-- Social buttons -->
+        <div class="text-center text-md-right">
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a target="_blank" href="https://www.facebook.com/coralify">
+			  <img alt="aCredu facebook" src="https://app.acredu.org/wp-content/uploads/2021/01/icon_facebook1.png" class="img-fluid align-middle w-50">
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a target="_blank" href="https://www.youtube.com/channel/UCa23OQNMhf81BZV7_whVfKg">
+			  <img alt="aCredu youtube" src="https://app.acredu.org/wp-content/uploads/2021/01/icon_youtube1.png" class="img-fluid align-middle w-50">
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a target="_blank" href=" https://www.linkedin.com/company/coralify">
+			  <img alt="aCredu linkedin" src="https://app.acredu.org/wp-content/uploads/2021/01/icon_linkedin1.png" class="img-fluid align-middle w-50">
+              </a>
+            </li>
+          </ul>
+        </div>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+</footer>
 </div>
+</body>
+</html>
 	
 
