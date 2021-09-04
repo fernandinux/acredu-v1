fd42f6835550fa7780ea2cd1b255cef779b15995
fernandinux <fernando.paca@gmail.com>
2021-09-03 21:33:51 -0500

    con template part 2

---
+++ template-parts/content-addcurso.php
@@ -0,0 +1,8 @@
+<div class="mx-3 my-5" style="max-width:300px">
+            <div class="card h-200 text-center py-5">
+                <a href="https://app.acredu.org/addcurso/?text-validate=<?php echo $posType ?>" ><i class="far fa-plus-square" style="font-size:10rem;color:#80808059"></i></a>
+                <div class="card-body">
+                    <h5 class="card-title">Agregar Curso</h5>                   
+                </div>
+            </div>
+</div>
