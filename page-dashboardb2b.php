f3dc7f7f57649d8898ed7b90026a34122e81cda3
fernandinux <fernando.paca@gmail.com>
2021-09-05 02:51:57 -0500

    enlaces  b2c y b2b por categoria name 14

--- page-dashboardb2b.php
+++ page-dashboardb2b.php
@@ -102,12 +102,7 @@
                         
         
         <!-- card -->
-<?php
-         query_posts( array(
-	'post_type'=> $posType,
-    'cat=62',
-    'posts_per_page' => -1,
-	 ));?>
+<?php query_posts(array('post_type'=>$posType, 'cat'=>'62', 'posts_per_page'=>'-1'));?>
 
        <!-- <?php //query_posts(array('post_type' => $posType ,'orderby' => 'DESC', 'posts_per_page' => -1)); ?> -->
                 <?php if(have_posts()) : while(have_posts()) : the_post();?>
