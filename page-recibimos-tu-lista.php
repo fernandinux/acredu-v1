6e4ed8d4615f4c5e75df388042c8baff3dfc4dd6
fernandinux <fernando.paca@gmail.com>
2021-09-16 13:59:17 -0500

    funtion addlista form solo crear cpt idcat en content y new page recibimos tu lista

---
+++ page-recibimos-tu-lista.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Draft-acredu
+ */
+
+get_header();
+?>
+<?php
+	get_template_part( 'template-parts/content', 'encabezado' );
+	?>
+	<div style="max-width:300px; margin:30px auto 10px auto">
+		<h4 style="text-align: center;"><strong>Recibimos la nueva lista del curso, muy pronto nuestro equipo lo procesará.</strong></h4>
+		<h4 style="text-align: center;"><strong>Gracias por usar aCredu.</strong></h4>
+		<p>&nbsp;</p>
+		<h1 style="text-align: center;"><a href="https://app.acredu.org/dashboard">Ir al dashboard</a></h1>
+		<p>&nbsp;</p>
+	</div>
+	
+<div class="verifooter">
+	<?php
+// get_sidebar();
+get_footer(); ?>
+</div>
