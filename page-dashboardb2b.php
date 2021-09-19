00c0b8169b73ea0d2afff1ab0c995f23c292c6d1
fernandinux <fernando.paca@gmail.com>
2021-09-19 17:29:39 -0500

    page de curso con listas 21

--- page-dashboardb2b.php
+++ page-dashboardb2b.php
@@ -122,7 +122,7 @@
                                 
                                 <p style="font-size:10px">Código:</p>
                                 
-                                <a href="<?php echo $urlparmetros; ?>">
+                                <a href="<?php $urlparmetros; ?>">
                                 <p class="card-title"><?php the_title(); ?></p> 
                                 </a>                               
                                 <p style="font-size:10px">Curso:</p> 
