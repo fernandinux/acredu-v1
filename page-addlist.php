15e9aa7770d6004336a94347f3ca7062da7c41ac
fernandinux <fernando.paca@gmail.com>
2021-09-19 22:49:08 -0500

    page lista back empresas

--- page-addlist.php
+++ page-addlist.php
@@ -26,7 +26,8 @@
 
     <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
-    <li class="breadcrumb-item"><a href="/dashboard">Panel</a></li>    
+    <li class="breadcrumb-item"><a href="/dashboard">Panel</a></li>
+     <li class="breadcrumb-item"><a href="/dashboardclient">B2B - Clientes Empresa</a></li>  
     <li class="breadcrumb-item"><a href="javascript:window.history.back();">Cursos de <?php echo $nameempresa; ?></a></li>
     <li class="breadcrumb-item active" aria-current="page">Nueva lista</li>
   </ol>
