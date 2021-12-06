7976da80f8345795ecd393f1e5fbfb4e175b9897
Fernandinux <fernando.paca@gmail.com>
2021-12-05 21:27:00 -0500

    add certificado3

--- single-castudio.php
+++ single-castudio.php
@@ -17,14 +17,14 @@
                     <!-- Inicio single -->
                     
 	<div class="row">		
-		<!-- <div class="col-md-12 pt-5 cloudy-knoxville-gradient">
-							
+		 <div class="col-md-12 pt-5 cloudy-knoxville-gradient">
+		<?php get_template_part( 'template-parts/content', 'certificate' );?>	
         	<?php //the_post_thumbnail( 'medium_large', array( 'class' => 'img-fluid shadow-lg mx-auto d-block animated zoomIn' )); ?>
-			<div style="display: flex;flex-direction: row;justify-content: center;align-items: center;margin: 20px auto 0px auto;">
+			<!--<div style="display: flex;flex-direction: row;justify-content: center;align-items: center;margin: 20px auto 0px auto;">
             <small>¿Algo salió mal con tu certificado?, escribenos:</small>
-            <a id="ayudabnp" target="_blank" href="https://bit.ly/2EIKmrc"><i class="fa fa-whatsapp faw"></i></a></div>			
-		</div> -->
-		<?php get_template_part( 'template-parts/content', 'certificate' );?>
+            <a id="ayudabnp" target="_blank" href="https://bit.ly/2EIKmrc"><i class="fa fa-whatsapp faw"></i></a></div>	-->		
+		</div> 
+		
         
 	</div>
 
