2365de69f0ac43d6fb01111af71d667e563e4d6d
Fernandinux <fernando.paca@gmail.com>
2021-12-05 22:23:03 -0500

    http acredu.app

--- template-parts/content-encabezado.php
+++ template-parts/content-encabezado.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-light ">
   <div class="container">
-    <a class="navbar-brand" href="https://acredu.org">
-      <img src="https://app.acredu.org/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="" loading="lazy" height="30">
+    <a class="navbar-brand" href="https://acredu.app">
+      <img src="https://acredu.app/wp-content/uploads/2021/01/Acredu_logo-final.png" alt="logo acredu" loading="lazy" height="30">
     </a>
     <?php
         if (is_user_logged_in()){
@@ -9,11 +9,11 @@
               <a class="nav-link btn btn-outline-success btn-sm" 
                   data-mdb-ripple-color="dark" 
                   style="color:#03aa99 ! important; border: 2px solid #03aa99 !important;" 
-                  href="https://app.acredu.org/solicitar-cambio-nombre">Soporte</a>
+                  href="https://acredu.app/solicitar-cambio-nombre">Soporte</a>
               <a class="nav-link btn btn-outline-success btn-sm" 
                   data-mdb-ripple-color="dark" 
                   style="color:#03aa99 ! important; border: 2px solid #03aa99 !important;" 
-                  href="https://app.acredu.org/?piereg_logout_url=true">Cerrar sesión</a>
+                  href="https://acredu.app/?piereg_logout_url=true">Cerrar sesión</a>
        <?php }
        ?>
 
